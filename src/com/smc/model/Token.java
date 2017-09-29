f90e78f51b8cb75377f5d599bfb2c247f16370e9
Stuart Clark <stuart.clark_45@live.co.uk>
2017-09-29 17:06:51 +0100

    Token: created

---
+++ src/com/smc/model/Token.java
@@ -0,0 +1,43 @@
+package com.smc.model;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Used to store a single token and the results of annotations obtained using
+ * 
+ * @author Stuart Clark
+ */
+@EqualsAndHashCode
+@ToString
+public class Token implements Serializable {
+
+  private static final long serialVersionUID = 1115320868857337412L;
+
+  @Getter
+  private String text;
+
+  /**
+   * The part of speech for the token. e.g. verb, noun etc.
+   */
+  @Getter
+  @Setter
+  private String pos;
+
+  @Getter
+  @Setter
+  private String ner;
+
+  private Token() {
+    // For morphia
+  }
+
+  public Token(String text) {
+    this.text = text;
+  }
+
+}
