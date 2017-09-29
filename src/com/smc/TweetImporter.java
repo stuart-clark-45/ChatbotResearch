fa24db6caa9a3f254e42044523493804f378aae8
Stuart Clark <stuart.clark_45@live.co.uk>
2017-09-29 17:05:51 +0100

    TweetImporter: using StatusAdapter

--- src/com/smc/TweetImporter.java
+++ src/com/smc/TweetImporter.java
@@ -14,8 +14,7 @@ import twitter4j.FilterQuery;
 import twitter4j.HashtagEntity;
 import twitter4j.StallWarning;
 import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
+import twitter4j.StatusAdapter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
@@ -39,62 +38,8 @@ public class TweetImporter {
     cb.setOAuthAccessToken("***REMOVED***");
     cb.setOAuthAccessTokenSecret("***REMOVED***");
 
-    StatusListener listener = new StatusListener() {
-
-      @Override
-      public void onStatus(Status status) {
-        // Get the user
-        User user = status.getUser();
-
-        // Get the hashtags
-        Set<String> hashtags = new HashSet<>();
-        for (HashtagEntity hashtag : status.getHashtagEntities()) {
-          hashtags.add(hashtag.toString());
-        }
-
-        // Build the tweet
-        Tweet tweet = new Tweet();
-        tweet.setText(status.getText());
-        tweet.setCreated(status.getCreatedAt());
-        tweet.setTweetId(status.getId());
-        tweet.setUserId(user.getId());
-        tweet.setUsername(user.getName());
-        tweet.setHashtags(hashtags);
-
-        // Save the tweet
-        MongoHelper.getDataStore().save(tweet);
-
-        // Logging
-        int count = counter.incrementAndGet();
-        if (count % 10 == 0) {
-          LOGGER.info(count + " tweets imported");
-        }
-      }
-
-      @Override
-      public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-
-      @Override
-      public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-
-      @Override
-      public void onScrubGeo(long userId, long upToStatusId) {}
-
-      @Override
-      public void onStallWarning(StallWarning warning) {
-        LOGGER.warn(warning.toString());
-        System.exit(0);
-      }
-
-      @Override
-      public void onException(Exception e) {
-        LOGGER.error("Error whilst streaming tweets", e);
-      }
-    };
-
-
     TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
-    twitterStream.addListener(listener);
+    twitterStream.addListener(new Listener());
 
     FilterQuery query = new FilterQuery().language("en").track("chatbot", "chatbots");
     twitterStream.filter(query);
@@ -102,6 +47,49 @@ public class TweetImporter {
 
     Thread.sleep(400000);
 
+  }
+
+  private class Listener extends StatusAdapter {
+    @Override
+    public void onStatus(Status status) {
+      // Get the user
+      User user = status.getUser();
+
+      // Get the hashtags
+      Set<String> hashtags = new HashSet<>();
+      for (HashtagEntity hashtag : status.getHashtagEntities()) {
+        hashtags.add(hashtag.toString());
+      }
+
+      // Build the tweet
+      Tweet tweet = new Tweet();
+      tweet.setText(status.getText());
+      tweet.setCreated(status.getCreatedAt());
+      tweet.setTweetId(status.getId());
+      tweet.setUserId(user.getId());
+      tweet.setUsername(user.getName());
+      tweet.setHashtags(hashtags);
+
+      // Save the tweet
+      MongoHelper.getDataStore().save(tweet);
+
+      // Logging
+      int count = counter.incrementAndGet();
+      if (count % 10 == 0) {
+        LOGGER.info(count + " tweets imported");
+      }
+    }
+
+    @Override
+    public void onStallWarning(StallWarning warning) {
+      LOGGER.warn(warning.toString());
+      System.exit(0);
+    }
+
+    @Override
+    public void onException(Exception e) {
+      LOGGER.error("Error whilst streaming tweets", e);
+    }
   }
 
   public static void main(String[] args) throws TwitterException, InterruptedException {
