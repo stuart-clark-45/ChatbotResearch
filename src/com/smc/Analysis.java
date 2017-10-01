abe1fdbb3c62c48740368c292785a1ff3c618890
Stuart Clark <stuart.clark_45@live.co.uk>
2017-10-01 17:41:38 +0100

    Analysis: write results to db

--- src/com/smc/Analysis.java
+++ src/com/smc/Analysis.java
@@ -3,9 +3,6 @@ package com.smc;
 import static org.mongodb.morphia.aggregation.Group.grouping;
 import static org.mongodb.morphia.query.Sort.descending;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -15,13 +12,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.candmcomputing.util.MongoHelper;
+import com.mongodb.DBCollection;
+import com.smc.model.HashTag;
+import com.smc.model.KeyPhrase;
+import com.smc.model.KeyWord;
 import com.smc.model.ParsedTweet;
+import com.smc.model.StringCountResult;
 
 public class Analysis {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(Analysis.class);
 
-  private static final String ELEMENT = "_id";
   private static final String COUNT = "count";
 
   private final Accumulator counter;
@@ -34,52 +35,35 @@ public class Analysis {
   }
 
   private void run() {
-    // Count the number of times that each hashtags appears and write the result to a file
-    writeResults(countDistinctElements("hashtags"), "hashtags.csv");
+    // Count the number of times that each hashtags appears and write the result to the database
+    countDistinctElements("hashtags", HashTag.class);
 
-    // Count the number of times that each keyword appears and write the results to a file
-    writeResults(countDistinctElements("keywords"), "keywords.csv");
+    // Count the number of times that each keyword appears and write the results to the database
+    countDistinctElements("keywords", KeyWord.class);
 
-    // Count the number of times that each phrase appears and write the results to a file
-    writeResults(countDistinctElements("keyphrases"), "keyphrases.csv");
+    // Count the number of times that each phrase appears and write the results to the database
+    countDistinctElements("keyphrases", KeyPhrase.class);
   }
 
   /**
    * Looks at all of the lists / sets which are assigned to {@code field} and counts the total
-   * number of times that each distinct element appears.
+   * number of times that each distinct element appears. Results are written to the database.
    *
    * @param field a field which has a list / set as a value.
-   * @return a {@link Iterator<HashMap>} sorted in descending order according to count. Each
-   *         {@link HashMap} in the {@link Iterator} has two keys:
-   *
-   *         "_id" => the distinct element that has been counted
-   *
-   *         COUNT => the number of times that the distinct element appeared in the database.
+   * @return a {@link Iterator<HashMap>} sorted in descending order according to count.
    */
-  private Iterator<HashMap> countDistinctElements(String field) {
-    return ds.createAggregation(ParsedTweet.class).unwind(field)
-        .group(field, grouping(COUNT, counter)).sort(descending(COUNT)).aggregate(HashMap.class);
-  }
+  private <T extends StringCountResult> void countDistinctElements(String field, Class<T> clazz) {
+    // Drop the collection
+    DBCollection collection = ds.getCollection(clazz);
+    collection.drop();
 
-  /**
-   * Writes the results to a csv file with the form: ELEMENT,COUNT
-   *
-   * @param results the results returned by calling {@link Analysis#countDistinctElements(String)}.
-   * @param filename the name of the file to save the results to.
-   */
-  private void writeResults(Iterator<HashMap> results, String filename) {
-    try {
-      // Write the results to a file
-      PrintWriter writer = new PrintWriter(filename, "UTF-8");
-      results.forEachRemaining(result -> {
-        writer.println(result.get(ELEMENT) + "," + result.get(COUNT));
-      });
-      writer.close();
 
-    } catch (FileNotFoundException | UnsupportedEncodingException e) {
-      LOGGER.error("Failed to write results to " + results, e);
-    }
-
+    // Perform aggregation and store result in database. Indexes are dropped before inserting into
+    // collection takes place to reduce aggregation time.
+    collection.dropIndexes();
+    ds.createAggregation(ParsedTweet.class).unwind(field).group(field, grouping(COUNT, counter))
+        .sort(descending(COUNT)).out(clazz);
+    ds.ensureIndexes(clazz);
   }
 
   public static void main(String[] args) {
