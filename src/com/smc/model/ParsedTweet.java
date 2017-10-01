d6efb9d5b3143f4003b7cefaa080fa800de98017
Stuart Clark <stuart.clark_45@live.co.uk>
2017-10-01 18:04:41 +0100

    ParsedTweet: javadoc

--- src/com/smc/model/ParsedTweet.java
+++ src/com/smc/model/ParsedTweet.java
@@ -4,11 +4,15 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.smc.util.Parser;
+
 import lombok.Getter;
 import lombok.Setter;
 
 /**
  * Module used to represent tweets in a way that enables analysis to be performed.
+ *
+ * @author Stuart Clark
  */
 public class ParsedTweet {
 
@@ -16,18 +20,31 @@ public class ParsedTweet {
   @Setter
   private String unparsed;
 
-  @Getter
-  @Setter
-  private Set<String> keywords;
 
-  @Getter
-  @Setter
-  private Set<String> keyphrases;
-
+  /**
+   * The set of hashtags that were used in the tweet.
+   */
   @Getter
   @Setter
   private Set<String> hashtags;
 
+  /**
+   * The key words for the tweet
+   */
+  @Getter
+  @Setter
+  private Set<String> keywords;
+
+  /**
+   * The key phrases for the tweet. These consist of both keywords and hashtags.
+   */
+  @Getter
+  @Setter
+  private Set<String> keyphrases;
+
+  /**
+   * The results of {@link Parser#getTokens()} for the tweet.
+   */
   @Getter
   @Setter
   private List<Token> tokens;
